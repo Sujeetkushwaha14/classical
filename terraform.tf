63eb8e205929d72735e2bf5098671c218997e087
sujeet kumar kushwaha <sujeetkushwaha14@gmail.com>
2025-07-21 13:23:23 +0530

    tested

---
+++ terraform.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = "4.36.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    
+  }
+  subscription_id = "ba3dd1c9-2ade-4648-a13f-65581e2a2874"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name = "rg-sujeet"
+  location = "westus"
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
